8e35c8d53eafa31f805da084f772b163322882d7
Rafael Couto <rafaelcsilva016@outlook.com>
2021-03-02 15:29:59 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
 import NavBarComponent from '../components/navBar';
 import MyInss from '../components/myInss';
 import Footer from '../components/footer';
+import Head from 'next/head';
 
 function Home() {
   return (
     <main>
+      <Head>
+        <link rel="shortcut icon" href="/images/inss-logo.png" type="image/x-icon"/>
+        <title>INSS</title>
+      </Head>
       <NavBarComponent />
       <MyInss />
       <Footer/>
